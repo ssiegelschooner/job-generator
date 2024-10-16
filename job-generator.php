8b1184fd373b6016ebac99d7ee9856ed809fe238
Stephen Siegel <steve@stephensiegel.net>
2024-10-15 20:22:15 -0400

    generator complete

--- job-generator.php
+++ job-generator.php
@@ -2,18 +2,18 @@
 /*
 Plugin Name: Job Post Generator
 Description: A plugin to generate random job posts with categories and custom fields via WP-CLI.
-Version: 1.1
+Version: 1.2
 Author: Schooner Strategies
 */
 
 if (defined('WP_CLI') && WP_CLI) {
     
     // Register WP-CLI command with the ability to specify the number of posts
-    // use like wp generate_job_posts --count=15
+    // use like wp generate_job_posts --count=150
     WP_CLI::add_command('generate_job_posts', function($args, $assoc_args) {
 
         // Load config from config.php
-        $config = include plugin_dir_path(__FILE__) . 'config.php';
+        $config = include plugin_dir_path(__FILE__) . 'job-generator-config.php';
 
         // Extract arrays from config
         $categories = $config['categories'];
@@ -38,7 +38,10 @@ if (defined('WP_CLI') && WP_CLI) {
         // Generate the specified number of job posts
         for ($i = 1; $i <= $num_posts; $i++) {
             // Randomly pick category, employment type, experience level, company, and location
-            $category = $categories[array_rand($categories)];
+            $category_key = array_rand($categories);
+            $category = $category_key;  // Use the key as category name
+            $job_titles = $categories[$category_key];  // Get the job titles for that category
+            $job_title = $job_titles[array_rand($job_titles)];  // Randomly pick a job title for the category
             $employment_type = $employment_types[array_rand($employment_types)];
             $experience_level = $experience_levels[array_rand($experience_levels)];
             $company = $companies[array_rand($companies)];
@@ -50,8 +53,8 @@ if (defined('WP_CLI') && WP_CLI) {
 
             // Create a new post
             $new_post = [
-                'post_title'    => "Job Position $i in $category",
-                'post_content'  => "This is a job posting for $category.",
+                'post_title'    => "$job_title at $company",
+                'post_content'  => "This is a job posting for the position of $job_title in $category at $company, located in $location.",
                 'post_status'   => 'publish',
                 'post_author'   => 1,
                 'post_category' => [wp_create_category($category)], // Create category if it doesn't exist
@@ -76,7 +79,7 @@ if (defined('WP_CLI') && WP_CLI) {
                 update_post_meta($post_id, 'location', $location);
             }
 
-            WP_CLI::success("Created post ID $post_id with title 'Job Position $i in $category'");
+            WP_CLI::success("Created post ID $post_id with title '$job_title at $company'");
         }
 
     }, [
